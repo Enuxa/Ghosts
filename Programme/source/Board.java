6fba9cea38007310005c47978d02465cc3c3af46
Enuxa <pixxy.en@gmail.com>
2015-11-29 17:38:52 +0100

    Ecriture des classes Board et Square

--- Programme/source/Board.java
+++ Programme/source/Board.java
@@ -1,2 +1,49 @@
+import java.util.*;
+
+/**
+ * Classe correspondant au plateau
+ */
 public class Board{
+	/**
+	 * @param size Nombre de cases sur le côté.
+	 */
+	private int size;
+	private Map<String, Square> squares;
+	public Board (int size){
+		this.size = size;
+		this.squares = new HashMap <String, Square> ();
+		for (int i = 0; i < this.size; i++){
+			for (int j = 0; j < this.size; j++){
+				String coordinates = Character.toString ((char)('a' + i)) + (j + 1);
+				this.squares.put (coordinates, new Square (coordinates));
+			}
+		}
+	}
+	/**
+	 * Récupère une case
+	 * @param coordinates Les coordonnées aphanumériques de la case souhaitée.
+	 * @return La case demandée.
+	 */
+	public Square getSquare (String coordinates){
+		return this.squares.get(coordinates);
+	}
+	/**
+	 * Récupère le nombre de case sur le côté.
+	 * @return La taille du côté du plateau.
+	 */
+	public int getSize (){
+		return this.size;
+	}
+	/**
+	 * Récupère la position d'un fantôme.
+	 * @param ghost Le fantôme dont on veut la position.
+	 * @return La position du fantôme passé en argument.
+	 */
+	public String getPosition (Ghost ghost){
+		Collection<String> positions = this.squares.keySet();
+		for (String s : positions)
+			if ((this.getSquare (s)).getGhost () == ghost)
+				return s;
+		return null;
+	}
 }
--- Programme/source/Square.java
+++ Programme/source/Square.java
@@ -1,2 +1,39 @@
+/**
+ *	Classe correspondant à une case.
+ */
 public class Square{
+	private String coordinates;
+	private Ghost ghost;
+	/**
+	 * @param coordinates Coordonnées de cette case.
+	 */
+	public Square (String coordinates){
+		this.coordinates = coordinates;
+	}
+	/**
+	 * Récupère le fantôme présent sur ctte case
+	 * @return Le fantôme sur cette case, ou <code>null</code> s'il n'y a aucun fantôme sur cette case.
+	 */
+	public Ghost getGhost (){
+		return this.ghost;
+	}
+	/**
+	 * Place un fantôme sur cette case.
+	 * @param ghost Le fantôme à placer.
+	 */
+	public void putGhost (Ghost ghost){
+		this.ghost = ghost;
+	}
+	/**
+	 * Retire le fantôme présent sur cette case.
+	 */
+	public void removeGhost (){
+		this.ghost = null;
+	}
+	/**
+	 * Récupère les coordonnées de cette case.
+	 */
+	public String toString (){
+		return this.coordinates;
+	}
 }
